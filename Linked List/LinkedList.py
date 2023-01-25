2371266a9dae1bbb19f9be257671c6cd9b50eb71
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2023-01-24 21:59:51 -0500

    Prepend Method added

--- Linked List/LinkedList.py
+++ Linked List/LinkedList.py
@@ -11,48 +11,66 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
     
+
     def print_list(self):
+        if self.head is None:
+            print("List is Empty")
+            return 
         temp = self.head
         while temp:
             print(temp.value)
             temp = temp.next
         print("length is : ", self.length)
 
+
     def append(self,value):
         node = Node(value)
         if self.head is None:
             self.head = node
             self.tail = node
-            self.length = 1
+            self.length += 1
         else:
-            
             self.tail.next = node
             self.tail = node
             self.length += 1
     
 
     def pop(self):
-        if self.head is None:
-            return 
-        elif self.head == self.tail:
-            self.head = None
-            self.tail = None
-            self.length = 0
+        if self.length == 0:
+            return None
         else:
             temp = self.head
-            while temp.next != self.tail:
+            pre = self.head
+            while temp.next:
+                pre = temp
                 temp = temp.next
-            temp.next = None
-            self.tail = temp
+               
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+
+
+    def prepend(self, value):
+        new_node = Node(value= value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
         
 
 mylist = LinkedList(4)
-mylist.append(5)
-mylist.append(3)
-mylist.append(6)
-mylist.pop()
+mylist.append(7)
 mylist.append(6)
 print("complete List \n")
+print("Before Prepand")
 mylist.print_list()
-print("\n Head is : ",mylist.head.value)
-print("\n Tail is : ",mylist.tail.value)
+mylist.prepend(1)
+print("\n\n After Prepand \n")
+mylist.print_list()
