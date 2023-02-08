6b41daaa2546d2548a0e3afe2d02e40be1f536b8
Vishal Barvaliya <vishalbarvaliya112@gmail.com>
2023-02-07 23:18:32 -0500

    Queue updated

--- Stack and Queue/Queue.py
+++ Stack and Queue/Queue.py
@@ -15,6 +15,33 @@ class queue:
         while temp:
             print(temp.value)
             temp = temp.next
+
+    def enqueue(self,value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last = new_node
+            self.length = 1
+        else:
+            self.last.next = new_node
+            self.last = self.last.next
+            self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return False
+        else:
+            temp = self.first
+            self.first = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+        
     
 a = queue(1)
+a.enqueue(2)
+a.enqueue(3)
+a.enqueue(4)
+a.dequeue()
+a.dequeue()
 a.print_queue()
